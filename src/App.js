a529048f472ff2bafc7e1c8cc9d13581492e7e44
americanman <snowboardracer7@gmail.com>
2018-10-25 08:10:09 -0400

    Delete default content

--- src/App.js
+++ src/App.js
@@ -2,45 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Car2 {
-  constructor(color) {
-    this.color = color;
-  }
-  getColor() {
-    this.getColor() {
-      return
-    }
-  }
-}
 
 class App extends Component {
-   
-//   Car = (color) => {
-//    this.color = color;
-//  }
-//   // we use prototype to create a method for a class in ES5
-//  Car.prototype.getColor = () => {
-//    return this.color
-//  }
-//
-// let my Car = new Car('red')
-
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>App component that inherits render method from React.Components</p>
       </div>
     );
   }
