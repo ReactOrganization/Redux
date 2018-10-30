229e3edfe8d1f793fb854fe0b7e6b631ad22ccc8
americanman <snowboardracer7@gmail.com>
2018-10-29 23:40:51 -0400

    Add body component

--- src/App.js
+++ src/App.js
@@ -2,44 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { createStore } from 'redux';
-
-let defaultState = {
-  originAmount: '0.00'
-}
-
-//reducer is a function that updates state and it takes two arguments state and action
-//(state = defaultState) is equivalent of ES5 state = state || defaultState - if state is defined use state if not, use dafaultState
-let amount = (state = defaultState, action) => {
-  if(action.type ==='CHANGE_ORIGIN_AMOUNT') {
-    //create a new object Object.assign({}) - becouse dirrect mutetig state is not allowed, then copy state and assign action.data to originAmount
-    //return  Object.assign({}), state, {originAmount : action.data} is equivalent of below where we use spread operator. spread operator returns new object
-    return {
-      ...state, originAmount : action.data
-    }
-  }
-
-  //else return defaul state
-  return state;
-}
-
-//store is where you save you state
-let store = createStore(amount);
-
-//subscribe watches for store update. store.getState() - a way we read the state
-store.subscribe(() => {
-  console.log('state', store.getState());
-})
-
-//to update store we use store.dispatch that fires an action and action must be an object
-store.dispatch({type: 'CHANGE_ORIGIN_AMOUNT', data:'300.65'})
-store.dispatch({type: ''})
-store.dispatch({type: ''})
+import Body from './components/body';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>App component that inherits render method from React.Components</p>
+        <Body />
+
       </div>
     );
   }
---
+++ src/components/body/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Body = () => {
+  return(
+    <div>
+      <p>some text</p>
+    </div>
+  )
+}
+
+export default Body
