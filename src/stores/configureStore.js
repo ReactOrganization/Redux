8ef697358d72ba6a9b857cb31b706402920d6188
americanman <snowboardracer7@gmail.com>
2018-10-29 23:32:59 -0400

    Move store to own folder

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,7 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { createStore } from 'redux';
+import { createStore } from 'redux'
 
 let defaultState = {
   originAmount: '0.00'
@@ -25,24 +22,4 @@ let amount = (state = defaultState, action) => {
 //store is where you save you state
 let store = createStore(amount);
 
-//subscribe watches for store update. store.getState() - a way we read the state
-store.subscribe(() => {
-  console.log('state', store.getState());
-})
-
-//to update store we use store.dispatch that fires an action and action must be an object
-store.dispatch({type: 'CHANGE_ORIGIN_AMOUNT', data:'300.65'})
-store.dispatch({type: ''})
-store.dispatch({type: ''})
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>App component that inherits render method from React.Components</p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default store;
